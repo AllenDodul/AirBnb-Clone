f06b38cea151015ddf86a5614911980d6bbe099e
AllenDodul <allendodul6@gmail.com>
2022-08-18 21:10:54 +0600

    2 files modified

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,8 +3,30 @@ import './style.css';
 
 const Product = () =>{
   return(
-    <div className="product">
-      Product
+    <div className="product-card">
+      <img className="product-image" 
+        src="https://i.ibb.co/6s8xqT2/images-68.jpg" 
+        alt="images-68" 
+      />
+      <div className="info">
+        <h4 className="name">Allen</h4>
+        <div className="details">
+          <img 
+            className="star-logo"
+            src="https://i.ibb.co/VNcchSr/Background-Eraser-20220818-205143670.png"
+            alt="Background-Eraser-20220818-205143670" 
+          />
+          <span className="gray">
+            (144)
+          </span>
+          <div>
+            <span className="price">
+              $250
+            </span>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
